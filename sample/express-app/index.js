ac3c49204ed0daeb769c12cf5803f352048dccf5
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-05-28 17:15:12 +0900

    Create sample of express-app

---
+++ sample/express-app/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const http = require('http')
+const server = http.createServer(app)
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+}))
+
+app.get('/',(req,res) => {
+  res.json({
+    message:"Hello Express App!!"
+  })
+})
+
+server.listen(5000,() => {
+  console.log('listening on \*:5000')
+})
